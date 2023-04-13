cc83d8d6679bc0df8020985f4a7eec5f44c10287
kusti420 <kristjan0@live.com>
2023-04-13 13:44:42 +0300

    idk

--- scrapers/youtube.py
+++ scrapers/youtube.py
@@ -1,15 +1,25 @@
 import os
 import pytube
 import validators
+from enum import Enum
 
 
 class youtubeScraper():
     def __init__(self, url):
         if validators.url(url):
             self.url = url
-        print("downloading: ", self.url)
+        # print("downloading: ", self.url)
+        self.download_options = Enum("type", ["video", "playlist", "channel"])
+        print(self.parser(self.url))
         pass
 
+    def parser(self, url):
+        # if url is of a video
+        if "watch" in url:
+            return self.download_options.video
+        if "playlist" in url:
+            return self.download_options.playlist
+        if "channel" in url:
+            return self.download_options.channel
+        pass
 
-pass
-# sc = youtubeScraper()

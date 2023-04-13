3dcb986347fd76a1cf478c2ea3b89cb753e908e1
Varun Banka <bankavarun18@gmail.com>
2023-04-13 16:26:46 +0530

    added support for downloading Metadata

--- scrapers/youtube.py
+++ scrapers/youtube.py
@@ -2,6 +2,7 @@ import os
 import pytube
 import validators
 from enum import Enum
+import json
 
 
 class youtubeScraper():
@@ -11,6 +12,11 @@ class youtubeScraper():
         # print("downloading: ", self.url)
         self.download_options = Enum("type", ["video", "playlist", "channel"])
         print(self.parser(self.url))
+
+        # Download video metadata if URL is of a video
+        if self.parser(self.url) == self.download_options.video:
+            self.download_metadata()
+
         pass
 
     def parser(self, url):
@@ -22,4 +28,15 @@ class youtubeScraper():
         if "channel" in url:
             return self.download_options.channel
         pass
+    
+    def download_metadata(self):
+        # Create a YouTube object and get the video metadata
+        youtube = pytube.YouTube(self.url)
+        metadata = youtube.player_response['videoDetails']
 
+        # Write the metadata to a JSON file
+        with open('video_metadata.json', 'w') as f:
+            json.dump(metadata, f)
+
+        print("Metadata downloaded successfully!")
+        pass
